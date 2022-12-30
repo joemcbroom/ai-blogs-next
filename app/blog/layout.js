abdf95a5bb31f267bade813de88846daca206eb9
Joe McBroom <joe.mcbroom@willowtreeapps.com>
2022-12-30 15:57:30 -0600

    test text

--- app/blog/layout.js
+++ app/blog/layout.js
@@ -6,6 +6,7 @@ const BLogLayout = ({ params, children }) => {
 	return (
 		<>
 			<nav className={styles.nav}>
+				<h1>Some links to blog posts:</h1>
 				<Link href="/blog/blog1/post1">Blog1 Post1</Link>
 				<Link href="/blog/blog1/post2">Blog1 Post2</Link>
 				<Link href="/blog/blog2/post1">Blog2 Post1</Link>
