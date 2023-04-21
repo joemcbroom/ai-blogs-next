cfdd0eea27fb9a117bae3bf72dd68e29559f7515
Joe McBroom <joe.mcbroom@willowtreeapps.com>
2023-04-21 16:42:59 -0400

    Remove unneccesary redirect

--- app/admin/settings/page.tsx
+++ app/admin/settings/page.tsx
@@ -5,15 +5,10 @@ import UserInfo from '#/components/admin/settings/UserInfo';
 // lib
 import { getProfile } from '#/lib/supabase/server';
 
-// framework
-import { redirect } from 'next/navigation';
-
 export const revalidate = 0;
 
 export default async function SettingsPage() {
 	const userProfile = await getProfile();
-
-	if (!userProfile) return redirect('/sign-in');
 
 	return (
 		<>
--- components/UI/loaders/Loader.tsx
+++ components/UI/loaders/Loader.tsx
@@ -1,7 +1,7 @@
 import IconLoader from './IconLoader';
 export default function Loader() {
 	return (
-		<div className="flex h-full items-center justify-center">
+		<div className="grid h-full place-items-center">
 			<IconLoader className="h-12 w-12" />
 		</div>
 	);
