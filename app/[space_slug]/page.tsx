2afaea4169abf4dea177f44f0a7b3f69946ba2e3
Joe McBroom <joe.mcbroom@willowtreeapps.com>
2023-06-02 17:08:22 -0400

    blog post page + routes

--- app/[space_slug]/page.tsx
+++ app/[space_slug]/page.tsx
@@ -1,7 +1,42 @@
-const BlogHome = async () => {
+import { supabase } from '#/lib/supabase/static';
+import { Post } from '#/lib/types/inferred.types';
+import Link from 'next/link';
+
+const getPosts = async (space_slug: string) => {
+	const { data: posts, error } = await supabase
+		.from('post')
+		.select(`*, space!inner(slug)`)
+		.eq('is_published', true)
+		.eq('space.slug', space_slug);
+
+	if (error) {
+		console.error(error);
+		throw error.message;
+	}
+
+	return posts as Post[];
+};
+const BlogHome = async ({
+	params: { space_slug },
+}: {
+	params: { space_slug: string };
+}) => {
+	const posts = await getPosts(space_slug);
+	if (!posts?.length) {
+		return <div>no (published) posts</div>;
+	}
+
 	return (
 		<>
-			<div>this is the BLOGS home page</div>;
+			<ol className="list-inside list-decimal p-4 text-xs">
+				{posts.map((post) => (
+					<li key={post.slug} className="my-2">
+						<Link href={`/${space_slug}/${post.slug}`} className="underline">
+							{post.title}
+						</Link>
+					</li>
+				))}
+			</ol>
 		</>
 	);
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,34 @@
-// components
-import Logout from '#/app/auth/Logout';
+import { supabase } from '#/lib/supabase/static';
+import { BlogSpace, BlogSpaceWithPosts } from '#/lib/types/inferred.types';
+import Link from 'next/link';
 
-// lib
-import { getAuthenticatedUser } from '#/lib/supabase/server';
+const getSpaces = async () => {
+	const { data: spaces, error } = await supabase
+		.from('space')
+		.select('*, posts: post(slug)')
+		.eq('is_published', true);
 
-export const dynamic = 'force-dynamic';
+	if (error) {
+		console.error(error);
+		throw error.message;
+	}
 
-export default async function Page() {
-	const user = await getAuthenticatedUser();
+	return spaces as BlogSpaceWithPosts[];
+};
+
+export default async function HomePage() {
+	const spaces = await getSpaces();
 	return (
 		<>
-			<h1 className="text-4xl font-bold">Home</h1>
-			{user && <Logout />}
+			<ol className="list-inside list-decimal p-4 text-xs">
+				{spaces.map((space) => (
+					<li key={space.slug} className="my-2">
+						<Link href={`/${space.slug}`} className="underline">
+							{space.title} ({space?.posts?.length || 0} posts)
+						</Link>
+					</li>
+				))}
+			</ol>
 		</>
 	);
 }
