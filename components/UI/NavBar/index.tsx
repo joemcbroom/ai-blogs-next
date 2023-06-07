048551f02fe58d432c987f8783ebabc8f39283d3
Joe McBroom <joe.mcbroom@willowtreeapps.com>
2023-06-07 15:49:19 -0400

    fix image optimization

--- components/UI/NavBar/index.tsx
+++ components/UI/NavBar/index.tsx
@@ -2,51 +2,12 @@ import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
 import BlogverseLogo from '../BlogverseLogo';
 import Link from 'next/link';
 import DarkToggle from './DarkToggle';
-
-const MobileBar = () => (
-	<div className="relative flex items-center justify-between px-4 py-2 md:hidden">
-		<BlogverseLogo
-			className="w-1/3 rounded-lg pt-1 dark:bg-white "
-			type="horizontal"
-			width={119}
-			height={27}
-		/>
-		<input
-			type="checkbox"
-			id="menu-toggle"
-			className="peer/menu-toggle hidden"
-		/>
-		<label
-			htmlFor="menu-toggle"
-			className="z-50 block peer-checked/menu-toggle:hidden"
-		>
-			<Bars3Icon className="h-6 w-6 " />
-		</label>
-		<label
-			htmlFor="menu-toggle"
-			className="z-50 hidden peer-checked/menu-toggle:block"
-		>
-			<XMarkIcon className="h-6 w-6 " />
-		</label>
-		{/* full height menu when menu-toggle is checked */}
-		<div className="absolute left-0 top-0 z-40 grid h-screen w-screen -translate-y-full place-items-center bg-neutral-100 transition-transform peer-checked/menu-toggle:translate-y-0 dark:bg-neutral-900">
-			<ul className="flex flex-col items-center space-y-4">
-				<li>
-					<Link href="/">Spaces</Link>
-				</li>
-				<li>
-					<Link href="/about">About Us</Link>
-				</li>
-				<DarkToggle />
-			</ul>
-		</div>
-	</div>
-);
+import MobileBar from './MobileBar';
 
 const DesktopBar = () => (
 	<div className="relative hidden items-center justify-between px-4 py-2 md:flex">
 		<BlogverseLogo
-			className="max-w-md rounded-lg pt-1 dark:bg-white"
+			className="max-w-md rounded-lg pt-1 dark:bg-neutral-50"
 			type="horizontal"
 			width={200}
 			height={45}
--- components/UI/ResponsiveImage.tsx
+++ components/UI/ResponsiveImage.tsx
@@ -12,22 +12,27 @@ const ResponsiveImage: React.FC<ResponsiveImageProps> = ({
 	height,
 	alt,
 	className,
-}) => (
-	<picture>
-		<source
-			srcSet={`${src}?width=${width}&height=${height}`}
-			media="(min-width: 1024px)"
-		/>
-		<source
-			srcSet={`${src}?width=${width * 0.75}&height=${height * 0.75}`}
-			media="(min-width: 640px)"
-		/>
-		<img
-			src={`${src}?width=${width * 0.3}&height=${height * 0.3}`}
-			alt={alt}
-			className={className}
-		/>
-	</picture>
-);
+}) => {
+	src = src.replace('/object/', '/render/image/');
+	return (
+		<>
+			<picture>
+				<source
+					srcSet={`${src}?width=${width}&height=${height}`}
+					media="(min-width: 1024px)"
+				/>
+				<source
+					srcSet={`${src}?width=${width * 0.75}&height=${height * 0.75}`}
+					media="(min-width: 640px)"
+				/>
+				<img
+					src={`${src}?width=${width * 0.3}&height=${height * 0.3}`}
+					alt={alt}
+					className={className}
+				/>
+			</picture>
+		</>
+	);
+};
 
 export default ResponsiveImage;
