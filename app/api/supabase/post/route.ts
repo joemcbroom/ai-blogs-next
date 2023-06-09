57b71db28acbe649ee3025ae844955153987dafb
joemcbroom <joe.mcbroom.10@gmail.com>
2023-06-09 15:10:37 -0400

    fix publish/unpublish

--- app/api/supabase/post/route.ts
+++ app/api/supabase/post/route.ts
@@ -21,6 +21,7 @@ export async function PUT(req: Request) {
 	const { error } = await supabase.from('post').update(data).eq('slug', slug);
 	if (error) throw error;
 
+	revalidatePath(`/${spaceSlug}`);
 	revalidatePath(`/${spaceSlug}/${slug}`);
 
 	return NextResponse.json({ success: true });
