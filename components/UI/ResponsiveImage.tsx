293d81dc97081a87077392727ab084556633e2a8
joemcbroom <joe.mcbroom.10@gmail.com>
2023-06-09 15:10:37 -0400

    add blur image

--- components/UI/ResponsiveImage.tsx
+++ components/UI/ResponsiveImage.tsx
@@ -7,6 +7,21 @@ interface ResponsiveImageProps {
 	alt: string;
 	className?: string;
 }
+const shimmer = (w: number, h: number) => `
+<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <linearGradient id="g">
+      <stop stop-color="#333" offset="20%" />
+      <stop stop-color="#222" offset="50%" />
+      <stop stop-color="#333" offset="70%" />
+    </linearGradient>
+  </defs>
+  <rect width="${w}" height="${h}" fill="#333" />
+  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+</svg>`;
+
+const toBase64 = (str: string) => Buffer.from(str).toString('base64');
 
 const ResponsiveImage: React.FC<ResponsiveImageProps> = ({
 	src,
@@ -23,6 +38,10 @@ const ResponsiveImage: React.FC<ResponsiveImageProps> = ({
 			height={height}
 			className={className}
 			style={{ objectFit: 'cover' }}
+			blurDataURL={`data:image/svg+xml;base64,${toBase64(
+				shimmer(height, width)
+			)}`}
+			placeholder="blur"
 		/>
 	);
 };
