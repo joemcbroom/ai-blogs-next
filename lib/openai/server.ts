fe4910333d648d8456c55be2cb6e9c753cb4ca63
joemcbroom <joe.mcbroom.10@gmail.com>
2023-06-09 15:09:45 -0400

    tiny css change

--- lib/openai/server.ts
+++ lib/openai/server.ts
@@ -1,4 +1,4 @@
-import { Configuration, OpenAIApi, ChatCompletionRequestMessage } from 'openai';
+import { Configuration, OpenAIApi } from 'openai';
 
 const config = new Configuration({
 	apiKey: process.env.OPENAI_API_KEY!,
