fbf2609e2f398d92eecbd94a09a51a48034b8ee9
joemcbroom <joe.mcbroom.10@gmail.com>
2023-06-09 15:09:43 -0400

    Remove pendulums viz

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,3 @@
-import PendulumsViz from '#/components/PendulumsViz';
-
 export default function Page() {
-	return (
-		<>
-			<PendulumsViz n={10} width={600} interval={100} />
-		</>
-	);
+	return <>home</>;
 }
