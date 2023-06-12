9ff5f8a2a69cd71f33954c81ce7fe8d304203f36
joemcbroom <joe.mcbroom.10@gmail.com>
2023-06-12 15:22:30 -0400

    testing metadatabase

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,7 +1,9 @@
 import NavBar from '#/components/UI/NavBar';
 
 export const metadata = {
-	metadataBase: new URL('ai-blogs-next-git-share-links-blogverse.vercel.app'),
+	metadataBase: new URL(
+		'https://ai-blogs-next-git-share-links-blogverse.vercel.app/'
+	),
 };
 
 type HomeLayoutProps = {
