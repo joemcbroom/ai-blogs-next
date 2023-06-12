75782b0f6a7ad50220f5b139ba55836c0214b61f
joemcbroom <joe.mcbroom.10@gmail.com>
2023-06-12 15:03:22 -0400

    fix og-image?

---
+++ app/(home)/[space_slug]/[post_slug]/twitter-image.tsx
@@ -0,0 +1,78 @@
+/* eslint-disable @next/next/no-img-element */
+import { getPost, supabase } from '#/lib/supabase/static';
+import { ImageResponse } from 'next/server';
+
+// Route segment config
+export const runtime = 'edge';
+
+// Image metadata
+export const alt = 'About Acme';
+export const size = {
+	width: 1200,
+	height: 630,
+};
+
+export const contentType = 'image/png';
+
+// Image generation
+export default async function Image({
+	params: { space_slug, post_slug },
+}: {
+	params: { space_slug: string; post_slug: string };
+}) {
+	const post = await getPost(post_slug);
+	let imageSrc = '';
+	const imagePath = post.image_path;
+	if (imagePath) {
+		const { data } = supabase.storage
+			.from('blogverse-public')
+			.getPublicUrl(imagePath || '', {
+				transform: {
+					width: 525,
+					height: 355,
+					resize: 'cover',
+				},
+			});
+		imageSrc = data?.publicUrl || '';
+	}
+	return new ImageResponse(
+		(
+			// ImageResponse JSX element
+			<div
+				style={{
+					fontSize: 128,
+					background: 'white',
+					width: '100%',
+					height: '100%',
+					display: 'flex',
+					alignItems: 'center',
+					justifyContent: 'end',
+				}}
+			>
+				<img
+					src={imageSrc}
+					alt=""
+					tw="flex-1 w-full h-full z-0"
+					style={{ objectFit: 'cover', objectPosition: 'center' }}
+				/>
+				<span
+					tw="flex-1 w-full z-10"
+					style={{
+						background: 'rgba(0,0,0,0.5)',
+						color: 'white',
+						fontSize: '2rem',
+						padding: '1rem',
+					}}
+				>
+					{post.title}
+				</span>
+			</div>
+		),
+		// ImageResponse options
+		{
+			// For convenience, we can re-use the exported opengraph-image
+			// size config to also set the ImageResponse's width and height.
+			...size,
+		}
+	);
+}
