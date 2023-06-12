4b59ff76efc6b88ac22813ad44fa1f8611d1911f
joemcbroom <joe.mcbroom.10@gmail.com>
2023-06-12 14:25:16 -0400

    add metadata to posts

--- app/(home)/[space_slug]/[post_slug]/layout.tsx
+++ app/(home)/[space_slug]/[post_slug]/layout.tsx
@@ -1,8 +1,41 @@
 import { supabase } from '#/lib/supabase/static';
+import { Post } from '#/lib/types/inferred.types';
 
 export const dynamic = 'force-static';
 
 export const revalidate = 30;
+
+const getPost = async (post_slug: string) => {
+	const { data: post, error } = await supabase
+		.from('post')
+		.select('*, space:space_id (image_path)')
+		.eq('slug', post_slug)
+		.single();
+
+	if (error) {
+		console.error(error);
+		throw error.message;
+	}
+
+	return post as Post;
+};
+
+type Props = {
+	params: { post_slug: string };
+};
+export async function generateMetadata({ params: { post_slug } }: Props) {
+	const post = await getPost(post_slug);
+	const { title, description } = post;
+
+	return {
+		title: {
+			template: `%s | Blogverse.ai`,
+			default: title,
+		},
+		description,
+		// keywords: tags.join(', '), TODO: get tags
+	};
+}
 
 export async function generateStaticParams({
 	params: { space_slug },
