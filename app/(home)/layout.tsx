88b77508ecd62ac96984a9b5615af0ebd74cb37f
joemcbroom <joe.mcbroom.10@gmail.com>
2023-06-12 15:56:27 -0400

    use NEXT_PUBLIC_VERCEL_URL

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -2,7 +2,7 @@ import NavBar from '#/components/UI/NavBar';
 
 export const metadata = {
 	metadataBase: new URL(
-		`https://${process.env.VERCEL_URL}` || 'http://localhost:3000'
+		`https://${process.env.NEXT_PUBLIC_VERCEL_URL}` || 'http://localhost:3000'
 	),
 };
 
