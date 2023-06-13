ded25fe8f700183ee65f1e2dbefcac7ef0ddad11
joemcbroom <joe.mcbroom.10@gmail.com>
2023-06-13 11:15:13 -0400

    postogimage component

---
+++ app/(home)/[space_slug]/[post_slug]/PostOgImage.tsx
@@ -0,0 +1,55 @@
+/* eslint-disable @next/next/no-img-element */
+const PostOgImage = ({
+	backgroundImageSrc,
+	title,
+}: {
+	backgroundImageSrc: string;
+	title: string;
+}) => {
+	return (
+		<div
+			style={{
+				backgroundImage: `url(${backgroundImageSrc})`,
+				height: '100%',
+				width: '100%',
+				display: 'flex',
+				position: 'relative',
+				backgroundSize: 'cover',
+			}}
+		>
+			<div
+				style={{
+					color: 'white',
+					textAlign: 'center',
+					backgroundColor: 'black',
+					padding: '3rem',
+					fontSize: 48,
+					fontWeight: 600,
+					position: 'absolute',
+					bottom: 0,
+					left: 0,
+					width: '100%',
+				}}
+			>
+				{title}
+			</div>
+			<img
+				style={{
+					backgroundColor: 'rgba(255, 255, 255, 0.75)',
+					borderRadius: '1rem',
+					width: '400px',
+					height: '120px',
+					position: 'absolute',
+					top: '1rem',
+					right: '1rem',
+					objectFit: 'contain',
+					padding: '0.75rem 0rem 0rem 0.5rem',
+				}}
+				src="https://dyhumgxwuzsrinvjiefx.supabase.co/storage/v1/render/image/public/blogverse-public/space/logo-blogverse-horiz.png?width=300&height=120&resize=contain"
+				alt="Blogverse logo"
+			/>
+		</div>
+	);
+};
+
+export default PostOgImage;
--- app/(home)/[space_slug]/[post_slug]/opengraph-image.tsx
+++ app/(home)/[space_slug]/[post_slug]/opengraph-image.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import { getPost, supabase } from '#/lib/supabase/static';
 import { ImageResponse } from 'next/server';
+import PostOgImage from './PostOgImage';
 
 // Route segment config
 export const runtime = 'edge';
@@ -23,66 +24,13 @@ export default async function Image({
 	const post = await getPost(post_slug);
 	const imagePath = post.image_path || post.space?.image_path;
 	const imageSrc = imagePath
-		? `https://dyhumgxwuzsrinvjiefx.supabase.co/storage/v1/render/image/public/blogverse-public/${imagePath}?width=500&height=300&resize=cover`
+		? `https://dyhumgxwuzsrinvjiefx.supabase.co/storage/v1/render/image/public/blogverse-public/${imagePath}?width=1200&height=600&resize=cover`
 		: '';
-	const logoSrc =
-		'https://dyhumgxwuzsrinvjiefx.supabase.co/storage/v1/render/image/public/blogverse-public/space/logo-blogverse-horiz.png?width=100&height=35&resize=contain';
 
 	return new ImageResponse(
 		(
 			// ImageResponse JSX element
-			<div
-				style={{
-					backgroundImage: `url(${imageSrc})`,
-					height: '100%',
-					width: '100%',
-					display: 'flex',
-					flexDirection: 'column',
-					alignItems: 'center',
-					justifyContent: 'flex-end',
-					fontSize: 28,
-					fontWeight: 600,
-					position: 'relative',
-					backgroundSize: 'cover',
-				}}
-			>
-				<div
-					style={{
-						color: 'white',
-						textAlign: 'center',
-						backgroundColor: 'black',
-						padding: '1rem',
-					}}
-				>
-					Tricks to Maximize Your Small-Space Vegetable Garden
-				</div>
-				<div
-					style={{
-						backgroundColor: 'rgba(255, 255, 255, 0.75)',
-						borderRadius: '1rem',
-						width: '120px',
-						height: '40px',
-						position: 'absolute',
-						top: '0.5rem',
-						right: '0.5rem',
-						display: 'flex',
-						alignItems: 'center',
-						justifyContent: 'center',
-						padding: '0.75rem 0rem 0rem 0.5rem',
-					}}
-				>
-					<div
-						style={{
-							width: '100%',
-							height: 35,
-							backgroundImage:
-								'url(https://dyhumgxwuzsrinvjiefx.supabase.co/storage/v1/render/image/public/blogverse-public/space/logo-blogverse-horiz.png?width=100&height=35&resize=contain)',
-							backgroundRepeat: 'no repeat',
-							backgroundSize: 'contain',
-						}}
-					/>
-				</div>
-			</div>
+			<PostOgImage backgroundImageSrc={imageSrc} title={post.title} />
 		),
 		// ImageResponse options
 		{
