0729ca809150ae4ca2e0f369ba595e8bda97702f
joemcbroom <joe.mcbroom.10@gmail.com>
2023-06-13 13:48:40 -0400

    update revalidate paths function

--- lib/utils/revalidatePaths.ts
+++ lib/utils/revalidatePaths.ts
@@ -1,7 +1,7 @@
 import { revalidatePath } from 'next/cache';
 
 export const revalidatePaths = () => {
-	revalidatePath('/(home)/[space_slug]');
-	revalidatePath('/(home)/[space_slug]/[post_slug]');
+	revalidatePath('/[space_slug]');
+	revalidatePath('/[space_slug]/[post_slug]');
 	revalidatePath('/');
 };
