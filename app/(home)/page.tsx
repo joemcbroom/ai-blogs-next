fb017dbbda1c33310facf67dea4a5c803ef1310e
joemcbroom <joe.mcbroom.10@gmail.com>
2023-06-13 14:05:34 -0400

    update revalidatePaths

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,22 +1,7 @@
-import { supabase } from '#/lib/supabase/static';
-import { BlogSpaceWithPosts } from '#/lib/types/inferred.types';
+import { getSpaces } from '#/lib/supabase/static';
 import Link from 'next/link';
 
-export const dynamic = 'force-dynamic';
-
-const getSpaces = async () => {
-	const { data: spaces, error } = await supabase
-		.from('space')
-		.select('*, posts: post(slug, is_published)')
-		.eq('is_published', true);
-
-	if (error) {
-		console.error(error);
-		throw error.message;
-	}
-
-	return spaces as BlogSpaceWithPosts[];
-};
+export const dynamic = 'force-static';
 
 export default async function HomePage() {
 	const spaces = await getSpaces();
--- lib/utils/revalidatePaths.ts
+++ lib/utils/revalidatePaths.ts
@@ -1,7 +1,8 @@
 import { revalidatePath } from 'next/cache';
 
 export const revalidatePaths = () => {
-	revalidatePath('/[space_slug]');
-	revalidatePath('/[space_slug]/[post_slug]');
+	revalidatePath('/(home)/[space_slug]');
+	revalidatePath('/(home)/[space_slug]/[post_slug]');
+	revalidatePath('/(home)');
 	revalidatePath('/');
 };
