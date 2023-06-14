2c01e5732bf40d4d309577f7dae9f6f1045a51a6
joemcbroom <joe.mcbroom.10@gmail.com>
2023-06-14 12:48:12 -0400

    remove unnecessary <></>

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -18,15 +18,13 @@ type HomeLayoutProps = {
 };
 const HomeLayout = async ({ children }: HomeLayoutProps) => {
 	return (
-		<>
-			<div
-				id="main-content-wrapper"
-				className="h-full bg-white text-black dark:bg-black dark:text-white"
-			>
-				<NavBar />
-				<main className="h-full">{children}</main>
-			</div>
-		</>
+		<div
+			id="main-content-wrapper"
+			className="h-full bg-white text-black dark:bg-black dark:text-white"
+		>
+			<NavBar />
+			<main className="h-full">{children}</main>
+		</div>
 	);
 };
 
