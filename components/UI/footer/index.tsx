19ae0126bef851d2b83a840af8228541689e5fa7
joemcbroom <joe.mcbroom.10@gmail.com>
2023-06-21 16:45:03 -0400

    fix cookie policy colors

--- components/UI/footer/index.tsx
+++ components/UI/footer/index.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 
 const Footer = () => {
 	return (
-		<footer className="flex flex-col items-center space-y-2 bg-slate-800 px-4 py-6 text-slate-200 dark:bg-slate-200 dark:text-slate-500">
+		<footer className="flex flex-col items-center space-y-2 bg-slate-800 px-4 py-6 text-slate-500 dark:bg-slate-200 dark:text-slate-500">
 			<BlogverseLogo type="grayHorizontal" width={180} height={32} />
 			<NavLinks />
 			<section className="mb-2 w-full border border-x-0 border-slate-50 p-4 text-center text-xs dark:border-slate-300">
