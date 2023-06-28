56ce1eb5ed825e43e47a28ad9012d5d79dd2d503
joemcbroom <joe.mcbroom.10@gmail.com>
2023-06-28 18:37:07 -0400

    add user images path

--- lib/constants/supabaseConstants.ts
+++ lib/constants/supabaseConstants.ts
@@ -4,6 +4,7 @@ const SUPABASE_CONSTANTS = {
 	PRIVATE_BUCKET: 'blogverse-private',
 	SPACE_IMAGES_PATH: 'space',
 	POST_IMAGES_PATH: 'post',
+	USER_IMAGES_PATH: 'user',
 	PUBLIC_URL(path: string) {
 		return `https://dyhumgxwuzsrinvjiefx.supabase.co/storage/v1/render/image/public/blogverse-public/${path}`;
 	},
